e951e33d68ce7db76cb6a7c68ba9a69ee8d9c6e8
KalinduBawantha <kalindubawantha@gmail.com>
2025-11-05 13:13:22 +0530

    thirdone

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter,Routes,Route} from "react-router-dom";
-//import Sidebar from "../components/Sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Mainpage from "./pages/Mainpage";
@@ -8,25 +7,22 @@ import ItemRegistration from "./pages/ItemRegistration";
 import Order from "./pages/Order";
 import OrderDelivery from "./pages/OrderDelivery";
 import ItemPriceRegistration from "./pages/ItemPriceRegistration";
-//import ManageItems from "./pages/ManageItems";
 
-// 
 function App() {
-  
   return (
-    <BrowserRouter>
-    <Routes>
-        <Route element={<Home/>} path="/"/>
-          <Route element={<Dashboard/>} path="/Dashboard"/>
-        <Route element={<Mainpage/>} path="/Mainpage"/>
-        <Route element={<Register/>} path="/Register"/>
-        <Route element={<ItemRegistration/>} path="/ItemRegistration"/>
-        <Route element={<Order/>} path="/Order"/>
-        <Route element={<OrderDelivery/>} path="/OrderDelivery"/>
-        <Route element={<ItemPriceRegistration/>} path="/ItemPriceRegistration"/>
-    </Routes>
+    <BrowserRouter basename="/Laundry-Management-System">
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Dashboard />} path="/Dashboard" />
+        <Route element={<Mainpage />} path="/Mainpage" />
+        <Route element={<Register />} path="/Register" />
+        <Route element={<ItemRegistration />} path="/ItemRegistration" />
+        <Route element={<Order />} path="/Order" />
+        <Route element={<OrderDelivery />} path="/OrderDelivery" />
+        <Route element={<ItemPriceRegistration />} path="/ItemPriceRegistration" />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
